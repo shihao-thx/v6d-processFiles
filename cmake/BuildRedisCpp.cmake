91aee6ba211c59ffbadbe8edc8f17a0bb5e422ae
shihao-thx <shihao@zju.edu.cn>
2022-07-21 10:05:55 +0800

    Support asynchronous

--- cmake/BuildRedisCpp.cmake
+++ cmake/BuildRedisCpp.cmake
@@ -16,7 +16,8 @@
 # maybe be used by dynamic lib
 set(REDIS_PLUS_PLUS_BUILD_TEST OFF CACHE BOOL "Build tests.")
 #set(REDIS_PLUS_PLUS_BUILD_SHARED OFF CACHE BOOL " Only build a static library.")
-set(hiredis_FOUND TRUE)
+set(REDIS_PLUS_PLUS_BUILD_ASYNC libuv CACHE STRING "Supports async interface")
+# 没有build静态库可能EXCLUDE_FROM_ALL的原因
 add_subdirectory_static(thirdparty/redis-plus-plus EXCLUDE_FROM_ALL)
 set(REDIS_PLUS_PLUS_LIBRARIES redis++)
 set(REDIS_PLUS_PLUS_INCLUDE_DIR thirdparty/redis-plus-plus/src/sw)
